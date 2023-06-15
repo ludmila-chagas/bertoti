9b297f5b77935b025416b1c0f7c777f8c657cb88
ludmila-chagas <ghansth@gmail.com>
2023-06-15 01:57:17 -0300

    Atualização

--- PadroesDeProjetos/Strategy/src/App.java
+++ PadroesDeProjetos/Strategy/src/App.java
@@ -5,12 +5,18 @@ public class App {
         Funcionario funcionarioSenior = new Funcionario("Maria", "456", new FuncionarioSenior(),
                 new FuncionarioSenior());
 
+        System.out.println("Conheça nosso quadro de funcionários\n");
+
+        System.out.println("Funcionário: " + funcionarioJunior.getNome());
         funcionarioJunior.receberSalario();
         funcionarioJunior.trabalhar();
         funcionarioJunior.atuarEmCargo();
+        System.out.println();
 
+        System.out.println("Funcionário: " + funcionarioSenior.getNome());
         funcionarioSenior.receberSalario();
         funcionarioSenior.trabalhar();
         funcionarioSenior.atuarEmCargo();
+
     }
 }
